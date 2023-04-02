9d6f747f39a01a746a4df3bb102d8c6b67f083c0
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-03 04:24:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,68 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
+import logging
+import asyncio
+from telethon import TelegramClient, events
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import (
+    ChannelParticipantsAdmins,
+    ChannelParticipantsKicked,
+    ChatBannedRights,
+)
 
-app = Client("my_bot", api_id=16844842, api_hash="f6b0ceec5535804be7a56ac71d08a5d4", bot_token="5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk")
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
-# define the /start command
-@app.on_message(filters.command("start"))
-def start_command(client: Client, message: Message):
-    message.reply_text("Hello! I'm ready to ban all members in your group. Use the /all command to begin.")
+# Set up the Telegram client
+api_id = 16844842
+api_hash = 'YOUR_API_HASH'
+bot_token = '5931504207:AAHf6b0ceec5535804be7a56ac71d08a5d4NzBcYEEX7AD29L0TqWF28axqivgoaKUk'
+client = TelegramClient('client_name', api_id, api_hash).start(bot_token=bot_token)
 
-# define the /all command
-@app.on_message(filters.command("all") & filters.group)
-def all_command(client: Client, message: Message):
-    # check if user is the bot owner
-    if message.from_user.id != 5148561602:
-        message.reply_text("You are not authorized to use this command.")
-        return
+# Set up the banned rights
+rights = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
 
-    # get the chat ID where the command was sent
-    chat_id = message.chat.id
+# Define the SUDO_USERS
+SUDO_USERS = [5148561602]
 
-    # check if bot is an admin in the chat
-    bot_member = client.get_chat_member(chat_id, "me")
-    if bot_member.status not in ["administrator", "creator"]:
-        bot_member = client.get_chat_member(chat_id, "me") # update bot_member
-        if bot_member.status not in ["administrator", "creator"]:
-            message.reply_text("I'm not an admin in this chat.")
-            return
+# Define the event handlers
+@client.on(events.NewMessage(pattern="^/kickall"))
+async def kickall(event):
+    if event.sender_id in SUDO_USERS:
+        # Get the admins in the chat
+        admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
+        admins_id = [i.id for i in admins]
+        # Iterate over the kicked users and kick them
+        async for user in event.client.iter_participants(event.chat_id, filter=ChannelParticipantsKicked):
+            if user.id not in admins_id:
+                await event.client.kick_participant(event.chat_id, user.id)
 
+@client.on(events.NewMessage(pattern="^/banall"))
+async def banall(event):
+    if event.sender_id in SUDO_USERS:
+        # Get the admins in the chat
+        admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
+        admins_id = [i.id for i in admins]
+        # Iterate over the kicked users and ban them
+        async for user in event.client.iter_participants(event.chat_id, filter=ChannelParticipantsKicked):
+            if user.id not in admins_id:
+                await event.client(EditBannedRequest(event.chat_id, user.id, rights))
 
-    # check if bot has permission to ban members
-    if not bot_member.can_restrict_members:
-        bot_member = client.get_chat_member(chat_id, "me") # update bot_member
-        if not bot_member.can_restrict_members:
-            message.reply_text("I don't have permission to ban members in this chat.")
-            return
+@client.on(events.NewMessage(pattern="^/unbanall"))
+async def unban(event):
+    if event.sender_id in SUDO_USERS:
+        # Iterate over the kicked users and unban them
+        async for user in event.client.iter_participants(event.chat_id, filter=ChannelParticipantsKicked):
+            await event.client(EditBannedRequest(event.chat_id, user.id, ChatBannedRights(until_date=None)))
+            await asyncio.sleep(0.1)
 
-    # get list of members in the chat and ban them
-    members = client.get_chat_members(chat_id)
-    for member in members:
-        try:
-            client.kick_chat_member(chat_id, member.user.id)
-        except Exception as e:
-            print(e) # handle any exceptions that may occur during the banning process
-
-    message.reply_text("All members have been banned from the chat.")
-
-app.run()
+# Start the client
+client.run_until_disconnected()
