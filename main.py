f297591e8ccb5c118dad0c463f019e4adfbc24ba
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-03 01:54:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
+import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
-import os
-import subprocess
-import urllib.parse
 
-# Replace the placeholders with your own values
-api_id = 16844842
-api_hash = "f6b0ceec5535804be7a56ac71d08a5d4"
-bot_token = "5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk"
+# Enter your API ID, API HASH, and BOT TOKEN below
+api_id = 16844842	
+api_hash = "f6b0ceec5535804be7a56ac71d08a5d4"	
+bot_token = "5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk"	
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-@app.on_message(filters.command(["start"]))
-def start_handler(_, message: Message):
-    message.reply_text("Hello! I am a music bot. Use /song to get audio of a song from YouTube, and /lyc to get lyrics.")
+# Define a function to handle the /start command
+@app.on_message(filters.command("start"))
+def start_command_handler(client: Client, message: Message):
+    # Send a welcome message to the user
+    client.send_message(chat_id=message.chat.id, text="Hello! Send me a /song command followed by the name of a song to get started.")
 
-@app.on_message(filters.command(["song"]))
-def song_handler(_, message: Message):
-    if len(message.command) == 1:
-        message.reply_text("Please specify a song name.")
-        return
-
+# Define a function to handle the /song command
+@app.on_message(filters.command("song"))
+def song_command_handler(client: Client, message: Message):
+    # Get the song name from the command arguments
     song_name = " ".join(message.command[1:])
 
-    # Download audio from YouTube using youtube-dl
-    audio_filename = f"{song_name}.mp3"
-    subprocess.run(["youtube-dl", "-x", "--audio-format", "mp3", "-o", audio_filename, f"ytsearch:{song_name}"])
+    # Use an API or library of your choice to search for the song and get its audio file
+    # For example, you could use the YouTube API and the pytube library to download a song as an mp3 file:
+    import pytube
+    video_url = pytube.YouTube(f"ytsearch:{song_name}").streams.filter(only_audio=True).first().url
+    audio_file = pytube.YouTube(video_url).streams.filter(only_audio=True).first().download()
+    
+    # Send the audio file to the user
+    client.send_audio(chat_id=message.chat.id, audio=audio_file)
+    
+    # Delete the downloaded file from the server to save storage space
+    os.remove(audio_file)
 
-    # Send audio to the user
-    message.reply_audio(audio_filename, title=urllib.parse.quote(song_name))
-
-    # Remove audio file
-    os.remove(audio_filename)
-
-@app.on_message(filters.command(["lyc"]))
-def lyc_handler(_, message: Message):
-    if len(message.command) == 1:
-        message.reply_text("Please specify a song name.")
-        return
-
-    song_name = " ".join(message.command[1:])
-
-    # Download lyrics from Genius using lyricsgenius
-    message.reply_text(f"Here are the lyrics for {song_name}: \n (Lyrics here...)")
-
+# Start the client
 app.run()
