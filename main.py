7366fc6c503078ca9ffe87a42d439056084abe2e
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-03 02:39:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pyrogram
 import re
 import requests
 from pytube import YouTube
-import musixmatch
 
 # Set up the Pyrogram client
 api_id = 16844842
@@ -11,20 +10,19 @@ api_hash = 'f6b0ceec5535804be7a56ac71d08a5d4'
 bot_token = '5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk'
 app = pyrogram.Client('my_bot', api_id, api_hash, bot_token=bot_token)
 
-# Set up the Musixmatch API client
-musixmatch_api_key = 'e86b5f48a332b3116e500790b650567c'
-musixmatch_api = musixmatch.WebService(apikey=musixmatch_api_key)
-
 # Define the song command handler
-@app.on_message(pyrogram.filters.command(['start', 'song', 'lyc']))
+@app.on_message(pyrogram.filters.command(['start', 'song']))
 def song_command_handler(client, message):
     # Define an empty string to hold the message text
     text = ''
 
     # Get the song name from the message text
+    song_name = message.text.split(' ', 1)[1]
+    
+    # Get the song name from the message text
     if message.command[0] == 'start':
         text = 'Welcome to my song bot!\nTo download a song, use the /song command followed by the name of the song.\nFor example, /song despacito'
-    elif message.command[0] == 'song':
+    else:
         song_name = message.text.split(' ', 1)[1]
         
         # Search for the song on YouTube and get the URL of the first video in the search results
@@ -35,55 +33,26 @@ def song_command_handler(client, message):
         
         # Download the audio and thumbnail of the video and send them to the user
         try:
-    if message.command[0] == 'vid':
-        video_url = message.command[1]
-        video = pafy.new(video_url)
-        bestaudio = video.getbestaudio()
-        audio_file_path = f"{video.title}.mp3"
-        thumbnail_file_path = f"{video.title}.jpg"
-        bestaudio.download(audio_file_path, quiet=True)
-        video.getbestthumb().download(thumbnail_file_path, quiet=True)
-        client.send_audio(
-            message.chat.id,
-            audio=open(audio_file_path, 'rb'),
-            thumb=open(thumbnail_file_path, 'rb'),
-            title=video.title,
-            performer=video.author
-        )
-    elif message.command[0] == 'lyc':
-        song_name = message.text.split(' ', 1)[1]
-        # Get the lyrics of the song using the Musixmatch API
-        try:
-            response = musixmatch_api.matcher_lyrics_get(song_name, '', 1)
-            track_id = response['message']['body']['track']['track_id']
-            lyrics = musixmatch_api.track_lyrics_get(track_id)['message']['body']['lyrics']['lyrics_body']
-            with open('lyrics.txt', 'w') as f:
-                f.write(lyrics)
-            client.send_document(
+            video = YouTube(video_url)
+            audio_stream = video.streams.filter(only_audio=True).first()
+            audio_stream.download(output_path='downloads/')
+            audio_file_path = os.path.join('downloads/', audio_stream.default_filename)
+            thumbnail_url = video.thumbnail_url
+            thumbnail_file_path = os.path.join('downloads/', f'{video.video_id}.jpg')
+            with open(thumbnail_file_path, 'wb') as f:
+                f.write(requests.get(thumbnail_url).content)
+            client.send_audio(
                 message.chat.id,
-                document=open('lyrics.txt', 'rb'),
-                caption=f"Lyrics of {response['message']['body']['artist_name']} - {response['message']['body']['track']['track_name']}"
+                audio=open(audio_file_path, 'rb'),
+                thumb=open(thumbnail_file_path, 'rb'),
+                title=video.title,
+                performer=video.author
             )
         except Exception as e:
             print(str(e))
             text = 'Sorry, an error occurred while processing your request.'
-    else:
-        text = 'Invalid command'
-except IndexError:
-    text = 'Please provide a valid command'
 
-try:
-    # If there is a message to send, send it
-    if 'text' in locals():
-        client.send_message(message.chat.id, text)
-finally:
-    # Clean up downloaded files
-    if 'audio_file_path' in locals():
-        os.remove(audio_file_path)
-    if 'thumbnail_file_path' in locals():
-        os.remove(thumbnail_file_path)
-    if 'lyrics.txt' in os.listdir():
-        os.remove('lyrics.txt')
+    client.send_message(message.chat.id, text)
 
-# Run the client
+# Start the Pyrogram client
 app.run()
