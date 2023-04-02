26dfa832368354c72d0100a28965cfb3941cbd6a
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-03 04:05:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,43 @@
-import pyrogram
 from pyrogram import Client, filters
-from pyrogram.errors import UserAdminInvalid, FloodWait
-import asyncio
-from pyrogram import filters
+from pyrogram.types import Message
 
-# Set up the Pyrogram client
-api_id = 16844842
-api_hash = 'f6b0ceec5535804be7a56ac71d08a5d4'
-bot_token = '5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk'
-bot = pyrogram.Client('my_bot', api_id, api_hash, bot_token=bot_token)
+app = Client("my_bot", api_id=16844842, api_hash="f6b0ceec5535804be7a56ac71d08a5d4", bot_token="5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk")
 
-# Create a Pyrogram client
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+# define the /start command
+@app.on_message(filters.command("start"))
+def start_command(client: Client, message: Message):
+    message.reply_text("Hello! I'm ready to ban all members in your group. Use the /all command to begin.")
 
-# Define the /start command handler
-@app.on_message(filters.command('start') & filters.private)
-async def start(bot, update):
-    await update.reply_text("Hello! I'm a group management bot. To use me, add me to a group and make me an admin. You can then use the /all command to ban all members in the group.")
-
-# Define the /all command handler
-@app.on_message(filters.command('all') & filters.group)
-async def ban_all_members(bot, update):
-    # Check if the command was sent by the bot owner or the group owner
-    if update.from_user.id not in (bot_owner_id, group_owner_id):
-        await update.reply_text("Sorry, only the bot owner or the group owner can use this command.")
+# define the /all command
+@app.on_message(filters.command("all") & filters.private)
+def all_command(client: Client, message: Message):
+    # check if user is the bot owner
+    if message.from_user.id != 5148561602:
+        message.reply_text("You are not authorized to use this command.")
         return
 
-    # Check if the bot is an admin of the group
-    try:
-        chat_member = await bot.get_chat_member(update.chat.id, "me")
-        if not chat_member.status in ("creator", "administrator"):
-            await update.reply_text("Sorry, I must be an admin of the group to use this command.")
-            return
-    except Exception as e:
-        print(e)
-        await update.reply_text("Sorry, an error occurred. Please try again later.")
+    # get the chat ID where the command was sent
+    chat_id = message.chat.id
+
+    # check if bot is an admin in the chat
+    bot_member = client.get_chat_member(chat_id, "me")
+    if not bot_member.status == "administrator":
+        message.reply_text("I'm not an admin in this chat.")
         return
 
-    # Ban all members in the group
-    members = []
-    async for member in bot.iter_chat_members(update.chat.id):
-        if member.user.is_bot or member.status in ("creator", "administrator"):
-            continue
+    # check if bot has permission to ban members
+    if not bot_member.can_restrict_members:
+        message.reply_text("I don't have permission to ban members in this chat.")
+        return
+
+    # get list of members in the chat and ban them
+    members = client.get_chat_members(chat_id)
+    for member in members:
         try:
-            await bot.kick_chat_member(update.chat.id, member.user.id)
-            members.append(member.user.id)
-        except UserAdminInvalid:
-            pass
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
+            client.kick_chat_member(chat_id, member.user.id)
         except Exception as e:
-            print(e)
-            continue
+            print(e) # handle any exceptions that may occur during the banning process
 
-    # Send confirmation message
-    if members:
-        text = f"{len(members)} members have been banned from the group."
-    else:
-        text = "No members were banned from the group."
-    await update.reply_text(text)
+    message.reply_text("All members have been banned from the chat.")
 
-# Define the on_chat_member_joined handler
-@app.on_chat_member_join()
-async def on_chat_member_joined(bot, update):
-    # Check if the bot was added to the group
-    if update.new_chat_members and bot.get_me().id in [user.id for user in update.new_chat_members]:
-        # Make the bot an admin of the group
-        await bot.promote_chat_member(update.chat.id, bot.get_me().id, can_delete_messages=True, can_restrict_members=True)
-        # Send a welcome message
-        await bot.send_message(update.chat.id, "Hello! I'm a group management bot. To use me, add me to a group and make me an admin. You can then use the /all command to ban all members in the group.")
-
-# Start the bot
-async def main():
-    global group_owner
+app.run()
