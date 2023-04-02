1d41534b9109e0129c2aa7a62583691deb1b3db6
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-03 02:05:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,46 +10,43 @@ api_hash = 'f6b0ceec5535804be7a56ac71d08a5d4'
 bot_token = '5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk'
 app = pyrogram.Client('my_bot', api_id, api_hash, bot_token=bot_token)
 
-# Define the start command handler
-@app.on_message(pyrogram.filters.command('start'))
-def start_command_handler(client, message):
-    client.send_message(
-        chat_id=message.chat.id,
-        text='Hello! Send me the name of a song and I will send you the audio file.'
-    )
-
 # Define the song command handler
-@app.on_message(pyrogram.filters.command('song'))
+@app.on_message(pyrogram.filters.command(['start', 'song']))
 def song_command_handler(client, message):
     # Get the song name from the message text
-    song_name = message.text.split(' ', 1)[1]
-    
-    # Search for the song on YouTube and get the URL of the first video in the search results
-    search_url = f'https://www.youtube.com/results?search_query={song_name}&sp=EgIQAQ%253D%253D'
-    html = requests.get(search_url).text
-    video_ids = re.findall(r"watch\?v=(\S{11})", html)
-    if video_ids:
+    if message.command[0] == 'start':
+        text = 'Welcome to my song bot!\nTo download a song, use the /song command followed by the name of the song.\nFor example, /song despacito'
+    else:
+        song_name = message.text.split(' ', 1)[1]
+        
+        # Search for the song on YouTube and get the URL of the first video in the search results
+        search_url = f'https://www.youtube.com/results?search_query={song_name}&sp=EgIQAQ%253D%253D'
+        html = requests.get(search_url).text
+        video_ids = re.findall(r"watch\?v=(\S{11})", html)
         video_url = f"https://www.youtube.com/watch?v={video_ids[0]}"
         
-        # Download the audio of the video and send it to the user
+        # Download the audio and thumbnail of the video and send them to the user
         try:
             video = YouTube(video_url)
             audio_stream = video.streams.filter(only_audio=True).first()
             audio_stream.download(output_path='downloads/')
             audio_file_path = os.path.join('downloads/', audio_stream.default_filename)
+            thumbnail_url = video.thumbnail_url
+            thumbnail_file_path = os.path.join('downloads/', f'{video.video_id}.jpg')
+            with open(thumbnail_file_path, 'wb') as f:
+                f.write(requests.get(thumbnail_url).content)
             client.send_audio(
                 message.chat.id,
                 audio=open(audio_file_path, 'rb'),
+                thumb=open(thumbnail_file_path, 'rb'),
                 title=video.title,
                 performer=video.author
             )
         except Exception as e:
             print(str(e))
-    else:
-        client.send_message(
-            chat_id=message.chat.id,
-            text='Sorry, I could not find the song you requested.'
-        )
+            text = 'Sorry, an error occurred while processing your request.'
+
+    client.send_message(message.chat.id, text)
 
 # Start the Pyrogram client
 app.run()
