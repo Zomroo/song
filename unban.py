9e61539f728e92b4a9209da92c6dec983e32f286
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-04 03:29:31 +0530

    Create unban.py

---
+++ unban.py
@@ -0,0 +1,31 @@
+import os
+from pyrogram import Client
+
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+# Get bot token and group ID from environment variables
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GROUP_ID = os.getenv("GROUP_ID")
+
+# Create a Pyrogram client instance
+app = Client("my_bot", bot_token=BOT_TOKEN)
+
+# Define function to unban all users in a group
+async def unban_all_users():
+    async with app:
+        # Get list of banned users in the group
+        banned_users = await app.get_chat_members(
+            chat_id=GROUP_ID,
+            filter="kicked"
+        )
+        # Unban each user
+        for user in banned_users:
+            await app.unban_chat_member(
+                chat_id=GROUP_ID,
+                user_id=user.user.id
+            )
+
+# Call the function to unban all users
+app.run(unban_all_users())
