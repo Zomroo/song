ea9d885fbaf8dcf368b9aa674bf9cf7fc0ecb350
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-04 04:05:50 +0530

    Update unban.py

--- unban.py
+++ unban.py
@@ -13,7 +13,7 @@ owner_id = 5148561602
 @app.on_message(pyrogram.filters.command("unbanall") & pyrogram.filters.user(owner_id))
 def unban_all(client, message):
     chat_id = message.chat.id
-    banned_users = client.get_chat_members(chat_id, filter="banned")
+    banned_users = client.get_chat_members(chat_id, filter=pyrogram.filters.ChatMembersFilterBanned())
     for user in banned_users:
         user_id = user.user.id
         client.unban_chat_member(chat_id, user_id)
