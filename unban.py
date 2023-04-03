c5191753f5394ef3ef18639efd30a059f1c028e1
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-04 03:34:47 +0530

    Update unban.py

--- unban.py
+++ unban.py
@@ -15,15 +15,26 @@ app = Client("my_bot", bot_token=BOT_TOKEN)
 # Define function to unban all users in a group
 async def unban_all_users():
     # Get list of banned users in the group
-    banned_users = app.iter_chat_members(
+    offset = 0
+    limit = 200
+    banned_users = await app.get_chat_members(
         chat_id=GROUP_ID,
-        filter="kicked"
+        filter="kicked",
+        offset=offset,
+        limit=limit
     )
-    # Unban each user
-    async for user in banned_users:
-        await app.unban_chat_member(
+    while banned_users:
+        for user in banned_users:
+            await app.unban_chat_member(
+                chat_id=GROUP_ID,
+                user_id=user.user.id
+            )
+        offset += limit
+        banned_users = await app.get_chat_members(
             chat_id=GROUP_ID,
-            user_id=user.user.id
+            filter="kicked",
+            offset=offset,
+            limit=limit
         )
 
 # Start the client and call the function to unban all users
