caea08cf98187bf04d5a4631024b59e131e6771c
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-04 04:04:37 +0530

    Update unban.py

--- unban.py
+++ unban.py
@@ -1,36 +1,22 @@
-from pyrogram import Client, filters
-from pyrogram.types import ChatPermissions, ChatMembersFilter
+import pyrogram
 
-# Add your own API ID and API Hash from the Telegram API website
-api_id = 16844842
-api_hash = 'f6b0ceec5535804be7a56ac71d08a5d4'
+app = pyrogram.Client(
+    "my_bot",
+    api_id=16844842,
+    api_hash="f6b0ceec5535804be7a56ac71d08a5d4",
+    bot_token="5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk"
+)
 
-# Add your own bot token obtained from the BotFather
-bot_token = '5931504207:AAHNzBcYEEX7AD29L0TqWF28axqivgoaKUk'
-
-# Add your own Telegram user ID as the owner ID
+# Replace "owner_id" with your Telegram user ID
 owner_id = 5148561602
 
-# create a Pyrogram client instance
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+@app.on_message(pyrogram.filters.command("unbanall") & pyrogram.filters.user(owner_id))
+def unban_all(client, message):
+    chat_id = message.chat.id
+    banned_users = client.get_chat_members(chat_id, filter="banned")
+    for user in banned_users:
+        user_id = user.user.id
+        client.unban_chat_member(chat_id, user_id)
+    message.reply_text("All banned users have been unbanned.")
 
-# define the command "/unbanall" and restrict it to group chats only
-@ app.on_message(filters.command("unbanall") & filters.group)
-def unban_all_members(client, message):
-    # check if the user is the owner of the bot
-    if message.from_user.id != owner_id:
-        message.reply_text("You do not have permission to unban all members.")
-        return
-
-    # retrieve the list of banned users
-    banned_members = client.get_chat_members(message.chat.id, filter=ChatMembersFilter.BANNED)
-
-    # unban each member one by one
-    for member in banned_members:
-        client.unban_chat_member(message.chat.id, member.user.id)
-
-    # notify the user that all members have been unbanned
-    message.reply_text("All members have been unbanned.")
-
-# start the bot
 app.run()
